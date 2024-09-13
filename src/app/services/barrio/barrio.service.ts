cc8be29ba3e323d50238dcfb0508eee6b8b39085
ElMelo9 <efmelo9@gmail.com>
2024-09-13 16:47:22 -0500

    crud barrio

--- src/app/services/barrio/barrio.service.ts
+++ src/app/services/barrio/barrio.service.ts
@@ -3,7 +3,7 @@ import { Observable } from 'rxjs';
 import { createHeaders } from '../../helpers/auth-helper';
 import { environment } from '../../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
-import { barrioResponse } from '../../models/barrio.interface';
+import { barrioResponse,barrioCreate,barrioUpdate } from '../../models/barrio.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +18,23 @@ export class BarrioService {
     const headers = createHeaders();
     const url = this.apiUrl + '/getAll'
     return this.http.get<barrioResponse[]>(url, { headers })
+  }
+
+  create(barrio: barrioCreate): Observable<barrioResponse> {
+    const headers = createHeaders();
+    const url = this.apiUrl + '/insert'
+    return this.http.post<barrioResponse>(url,barrio,{headers})
+  }
+
+  updateById(id: number, barrio: barrioUpdate): Observable<barrioResponse> {
+    const headers = createHeaders();
+    const url = this.apiUrl + '/update/' + id
+    return this.http.put<barrioResponse>(url, barrio, { headers })
+  }
+
+  deleteById(id: number): Observable<boolean> {
+    const headers = createHeaders();
+    const url = this.apiUrl + '/delete/' + id
+    return this.http.delete<boolean>(url, { headers })
   }
 }
