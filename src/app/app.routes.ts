9aca5bf5c9b7b3bd691b4e8ca98a376a6a60cba7
ElMelo9 <efmelo9@gmail.com>
2024-09-13 15:55:31 -0500

    add routeRoles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { LoginComponent } from './views/login/login.component';
 import { UsersComponent } from './views/users/users.component';
+import { RolesComponent } from './views/roles/roles/roles.component';
 
 export const routes: Routes = [
     {path:'home',component: HomeComponent},
     {path:'login',component: LoginComponent},
     {path:'users',component: UsersComponent},
+    {path:'roles',component: RolesComponent},
     { path: '', redirectTo: '/login', pathMatch: 'full' },  // Redirigir a login por defecto
     { path: '**', redirectTo: '/login' }  // Redirigir a login si la ruta no existe
 ];
